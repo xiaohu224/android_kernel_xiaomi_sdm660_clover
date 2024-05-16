4cdea1c073d0fb2dc014ab88509d009e6fbe17b5
Tim Zimmermann <tim@linux4.de>
2024-05-16 05:56:11 +0200

    syscall: Increase bpf fake uname to 5.4.186
    * https://android-review.googlesource.com/c/platform/packages/modules/Connectivity/+/3088785
    * Spoof for uprobestats as well
    Change-Id: Iaba91f5594cebd2e361b670fb866abb5c58c6707

--- kernel/sys.c
+++ kernel/sys.c
@@ -1150,8 +1150,9 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	memcpy(&tmp, utsname(), sizeof(tmp));
 	if (!strncmp(current->comm, "bpfloader", 9) ||
 	    !strncmp(current->comm, "netbpfload", 10) ||
-	    !strncmp(current->comm, "netd", 4)) {
-		strcpy(tmp.release, "4.19.236");
+	    !strncmp(current->comm, "netd", 4) ||
+	    !strncmp(current->comm, "uprobestats", 11)) {
+		strcpy(tmp.release, "5.4.186");
 		pr_debug("fake uname: %s/%d release=%s\n",
 			 current->comm, current->pid, tmp.release);
 	}
