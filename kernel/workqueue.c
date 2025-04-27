d4cd111ab93ffd959dd99ac48d53a225115b1acf
jsdizkcksv <3531236823@qq.com>
2025-04-27 17:51:13 +0800

    Revert "workqueue: Add RCU annotation for pwq list walk"
    This reverts commit 0da7c3b8d870e59da6542ee2be3a0fbcaf529b5f.

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -408,8 +408,7 @@ static void workqueue_sysfs_unregister(struct workqueue_struct *wq);
  * ignored.
  */
 #define for_each_pwq(pwq, wq)						\
-	list_for_each_entry_rcu((pwq), &(wq)->pwqs, pwqs_node,		\
-				lockdep_is_held(&wq->mutex))		\
+	list_for_each_entry_rcu((pwq), &(wq)->pwqs, pwqs_node)		\
 		if (({ assert_rcu_or_wq_mutex(wq); false; })) { }	\
 		else
 
